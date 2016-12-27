08567744a5873e2e6c74e3139784520055ccd888
SangHwaLee <SangHwaLee@DESKTOP-I7B0VUN>
2016-12-27 20:02:20 +0900

    RecipeImageService

---
+++ noodleBoard/src/main/java/org/noodle/service/RecipeImageServiceImpl.java
@@ -0,0 +1,32 @@
+package org.noodle.service;
+
+import org.noodle.domain.RecipeImageVO;
+import org.noodle.persistence.RecipeImageDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeImageServiceImpl implements RecipeImageService {
+
+	@Autowired
+	private RecipeImageDAO dao;
+	
+	@Override
+	public void regist(RecipeImageVO vo) throws Exception {
+			
+		dao.create(vo);
+	}
+
+	@Override
+	public RecipeImageVO view(Integer ino) throws Exception {
+
+		return dao.read(ino);
+	}
+
+	@Override
+	public void remove(Integer ino) throws Exception {
+
+		dao.delete(ino);
+	}
+
+}
