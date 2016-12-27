72e272d6a200cd6d8cc428592e7704e2768699b9
LEE <LEE@DESKTOP-QDIM0R1>
2016-12-27 12:30:50 +0900

    memberDAO, Impl

---
+++ noodleBoard/src/main/java/org/noodle/persistence/MemberDAO.java
@@ -0,0 +1,13 @@
+package org.noodle.persistence;
+
+import org.noodle.domain.MemberVO;
+
+public interface MemberDAO {
+
+	public void create(MemberVO vo)throws Exception;
+	public MemberVO read(int mno)throws Exception;
+	public void update(MemberVO vo)throws Exception;
+	public void delete(int mno)throws Exception;
+	
+	
+}
---
+++ noodleBoard/src/main/java/org/noodle/persistence/MemberDAOImpl.java
@@ -0,0 +1,42 @@
+package org.noodle.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.noodle.domain.MemberVO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+
+	@Inject
+	private SqlSession session;
+	
+	private static final String namespace = "org.noodle.mapper.MemberMapper";
+	
+	
+	@Override
+	public void create(MemberVO vo) throws Exception {
+
+		session.insert(namespace+".create",vo);
+	}
+
+	@Override
+	public MemberVO read(int mno) throws Exception {
+
+		return session.selectOne(namespace+".read",mno);
+	}
+
+	@Override
+	public void update(MemberVO vo) throws Exception {
+
+		session.update(namespace+".update",vo);
+	}
+
+	@Override
+	public void delete(int mno) throws Exception {
+
+		session.delete(namespace+".delete",mno);
+	}
+
+}
