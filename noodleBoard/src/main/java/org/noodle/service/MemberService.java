88611fd71e58c200d0d63800d2806686e75c4e66
LEE <LEE@DESKTOP-QDIM0R1>
2016-12-27 12:37:20 +0900

    MemberSerivce, Impl

--- noodleBoard/src/main/java/org/noodle/service/MemberService.java
+++ noodleBoard/src/main/java/org/noodle/service/MemberService.java
@@ -1,5 +1,12 @@
 package org.noodle.service;
 
+import org.noodle.domain.MemberVO;
+
 public interface MemberService {
 
+	public void create(MemberVO vo)throws Exception;
+	public MemberVO read(int mno)throws Exception;
+	public void update(MemberVO vo)throws Exception;
+	public void delete(int mno)throws Exception;
+	
 }
