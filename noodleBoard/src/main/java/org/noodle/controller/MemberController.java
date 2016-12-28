84aabd2b9d75b280e9e85058bb6b230d1aba4b53
LEE <LEE@DESKTOP-QDIM0R1>
2016-12-28 10:03:28 +0900

    Member con, mapper, register.jsp

--- noodleBoard/src/main/java/org/noodle/controller/MemberController.java
+++ noodleBoard/src/main/java/org/noodle/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class MemberController {
@@ -21,7 +21,9 @@ public class MemberController {
 	
 	@Inject
 	private MemberService service;
-	//seungwoo pratice
+	
+	
+	
 	@GetMapping("user/login")
 	public void login() throws Exception{
 		
@@ -36,17 +38,14 @@ public class MemberController {
 	}
 
 	@RequestMapping(value = "user/register", method = RequestMethod.POST)
-	public String registPOST(MemberVO vo, RedirectAttributes rttr) throws Exception {
+	public String registPOST(MemberVO vo) throws Exception {
 
 		logger.info("regist post....");
 		logger.info(vo.toString());
 
 		service.regist(vo);
 
-		rttr.addFlashAttribute("msg", "SUCCESS");
-
-		// return "/board/success";
-		return "redirect:index";
+		return "redirect:/user/login";
 	}
 	
 	
