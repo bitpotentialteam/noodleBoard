3850ea339d758f03340ac0d2dcadc16b598eb0e3
SangHwaLee <SangHwaLee@DESKTOP-I7B0VUN>
2016-12-28 16:46:45 +0900

    RecipeCuisineDAO, Mapper

--- noodleBoard/src/main/java/org/noodle/persistence/RecipeBoardDAO.java
+++ noodleBoard/src/main/java/org/noodle/persistence/RecipeBoardDAO.java
@@ -7,6 +7,7 @@ import org.noodle.domain.RecipeBoardVO;
 
 public interface RecipeBoardDAO {
 
+	
 	public void create(RecipeBoardVO vo) throws Exception;
 	public RecipeBoardVO read(int bno)throws Exception;
 	public void update(RecipeBoardVO vo)throws Exception;
@@ -14,9 +15,11 @@ public interface RecipeBoardDAO {
 	public List<RecipeBoardVO> listAll()throws Exception;
 	public List<RecipeBoardVO> search(Criteria cri)throws Exception;
 	
+	
 	public void addViewCount(Integer bno) throws Exception;
 	public void addLikeCount(Integer bno) throws Exception;
 	public void minusLikeCount(Integer bno) throws Exception;
 	public void addReplyCount(Integer bno) throws Exception;
 	public void minusReplyCount(Integer bno) throws Exception;
+	
 }
