80cca30a8ae20615e7d88d1225a49735528aa5c2
cookie <cookie@cookie>
2016-12-28 12:44:51 +0900

    wiki

---
+++ noodleBoard/src/main/java/org/noodle/controller/wikiController.java
@@ -0,0 +1,12 @@
+package org.noodle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wiki/*")
+public class wikiController {
+	
+	
+
+}
