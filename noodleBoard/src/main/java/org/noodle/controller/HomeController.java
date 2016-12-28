06e2212d99b4e437bbb9e2571325ff32df8cff00
Amber KO <amber.daeun90@gmail.com>
2016-12-28 11:25:04 +0900

    Controller modified

--- noodleBoard/src/main/java/org/noodle/controller/HomeController.java
+++ noodleBoard/src/main/java/org/noodle/controller/HomeController.java
@@ -1,18 +1,36 @@
 package org.noodle.controller;
 
+import javax.inject.Inject;
+
+import org.noodle.service.TimeLineService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Inject
+	TimeLineService service;
 
 	@GetMapping("/")
 	public String home()throws Exception{
 		logger.info("index called...");
 		return "index";
+	}
+	
+	
+	@GetMapping("/timeline")
+	public String timeLine(Model model) throws Exception {
+
+		logger.info("getTIMELINE.....");
+
+		model.addAttribute("list", service.listView());
+
+		return "timeline/timeline";
 	}
 }
