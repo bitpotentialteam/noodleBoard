8a743ba78d9b079229031884c2979b4820b627a1
LEE <LEE@DESKTOP-QDIM0R1>
2017-02-08 12:44:52 +0900

    시큐리티

--- noodleBoard/src/main/java/org/noodle/security/SuccessHandler.java
+++ noodleBoard/src/main/java/org/noodle/security/SuccessHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.Response;
@@ -41,7 +42,7 @@ public class SuccessHandler extends SavedRequestAwareAuthenticationSuccessHandle
 		MemberVO memberVO = null;
 		try {
 			memberVO = mservice.read1(user.getUsername());
-
+			
 			request.getSession().setAttribute("memberVO", memberVO);
 
 		} catch (Exception e) {
